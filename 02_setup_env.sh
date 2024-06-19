de8ea4ff1f84494c283ffbff45dd06183358e506
mymy20220326 <mymy20220326@outlook.com>
2024-06-19 15:32:53 +0800

    2024_06_19_0220-wmtag_memo_02_脚本_cloudstudio_运行_继续完善

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+f23_install_some_software(){
+	if [[ -f $(which cloudstudio) ]]; then
+		apt update -y
+		DEBIAN_FRONTEND=noninteractive apt install -y \
+			iputils-ping \
+			make \
+			lsof \
+			net-tools \
+			psmisc \
+			file \
+			rsync \
+			curl \
+			direnv \
+			sqlite3 \
+			libsqlite3-dev \
+			bats \
+			jq \
+			gron \
+			pdfgrep \
+			ripgrep \
+			httpie \
+			duff \
+			mlocate \
+      pv
+
+	fi
+
+	return 0
+}
+
 f36_install_gcc_10_versioin(){
   apt install -y gcc-10
   apt install -y g++-10
@@ -13,34 +43,47 @@ f36_install_gcc_10_versioin(){
 }
 
 l22_apt_install_software(){
+
   sudo apt update
 
   # NOTE ch07等多个章节
   # /usr/bin/ld: cannot find -lgdbm_compat
-  sudo apt update
   sudo apt install -y libgdbm-dev
   sudo apt install -y libgdbm-compat-dev
 
   # NOTE ch08章节需要的
-  sudo apt install libmysqlclient-dev
+  sudo apt install -y libmysqlclient-dev
 
   # NOTE ch10章节需要的
-  sudo apt-get -y install electric-fence
+  sudo apt install -y electric-fence
 
   # NOTE ch12章节需要的
-  sudo apt-get -y install libpthread-dev
+  # sudo apt-get -y install libpthread-dev
+
+  return 0
+}
+
+f94_2828_30_main(){
+
+	if [[ -f $(which cloudstudio) ]]; then
+
+    f23_install_some_software
+
+    f36_install_gcc_10_versioin
+
+    l22_apt_install_software
+
+  fi
 
   return 0
 }
 
 main(){
 
-  f36_install_gcc_10_versioin
-
-  l22_apt_install_software
+  f94_2828_30_main
 
   return 0
 }
 
-main
-
+# 下面是_正式_的入口
+[ -z "$1" ] && eval f94_2828_30_main || eval $1 $*
