3d99e664db6df744a577876ba1b69b324d2d229b
mymy20220326 <mymy20220326@outlook.com>
2024-07-11 16:10:49 +0800

    2024_07_11_0500-wmtag_memo_合并_data_祝贺古文诗词_80首_文本与拼音

---
+++ data/cs36_learning_grade/gs74_祝贺古文诗词_80首_拼音_club_终端/bg58_随机选择一个古诗展现其内容.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+# 定义一个函数,用于随机选择并显示一个_祝贺古诗_文本文件的内容
+gs30_bg58_30_show_random_gs_text_file() {
+    # 获取所有匹配sg*.txt模式的文件列表
+    files=($(ls sg*.txt))
+    
+    # 检查是否有匹配的文件
+    if [ ${#files[@]} -eq 0 ]; then
+        echo "没有发现古诗文本文件."
+        return 1
+    fi
+    
+    # 随机选择一个文件
+    random_index=$((RANDOM %${#files[@]}))
+    selected_file="${files[$random_index]}"
+    
+    # 显示选中的文件内容
+    cat "$selected_file"
+}
+
+# 调用函数
+gs30_bg58_30_show_random_gs_text_file
